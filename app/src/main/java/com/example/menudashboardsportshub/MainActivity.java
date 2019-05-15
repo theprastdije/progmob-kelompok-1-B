74471417fad47c10a14171d05ab6ca135c661a58
yehezkielrivaldoputra <ziel_X@yahoo.com>
2019-05-15 20:01:05 +0800

    tidak ada perubahan, commit ulang agar tidaak terjadi error

--- app/src/main/java/com/example/menudashboardsportshub/MainActivity.java
+++ app/src/main/java/com/example/menudashboardsportshub/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.menudashboardsportshub;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list.addAll(BasketData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        AlertDialog.Builder rvCategory;
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListBasketAdapter listBasketAdapter = new ListBasketAdapter(this);
+        listBasketAdapter.setListBasket(list);
+        rvCategory.setAdapter(listBasketAdapter);
     }
 }
